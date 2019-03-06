da7c6108e457a0354f4f61afec13858a2353956d
João Felipe Alves <joao@intransitorio.com.br>
2019-03-06 01:34:15 -0300

    CRA hello world

---
+++ precache-manifest.21c914ea6a5565039702344e66b7f84b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6552783b16c5a93c751",
+    "url": "/static/css/main.a35e5fc0.chunk.css"
+  },
+  {
+    "revision": "a6552783b16c5a93c751",
+    "url": "/static/js/main.a6552783.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/static/js/2.a539ea9d.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8f0945491f312e069c672dbbc66eefa1",
+    "url": "/index.html"
+  }
+];
