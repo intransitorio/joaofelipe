fea35fab6a7b6556e61a47b4d46a59e6d42dc607
João Felipe Alves <joao@intransitorio.com.br>
2019-08-15 22:43:15 -0300

    Updates

---
+++ precache-manifest.45288c42d650bb7316c80bc894932bb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a048050e23e9cc39cdc1",
+    "url": "/joaofelipe/static/css/main.a35e5fc0.chunk.css"
+  },
+  {
+    "revision": "a048050e23e9cc39cdc1",
+    "url": "/joaofelipe/static/js/main.a048050e.chunk.js"
+  },
+  {
+    "revision": "07091ec7332101d224aa",
+    "url": "/joaofelipe/static/js/runtime~main.07091ec7.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/joaofelipe/static/js/2.a539ea9d.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/joaofelipe/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "09fb57cd44fb3df8d599972e5d92cf11",
+    "url": "/joaofelipe/index.html"
+  }
+];
